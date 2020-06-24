c1dc9f3a3e920815308195104d7233bce96c02a8
Uprofound <uzunmarina@mail.ru>
2020-06-24 23:55:31 +0400

    RoR_lesson5_classwork (Scaffold)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
+# resources :posts, only: [:index, :show]
+# resources :posts, except: [:destroy]
   get 'static_pages/about'
   root 'home#index'
+  get '/home(/:hello)', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
