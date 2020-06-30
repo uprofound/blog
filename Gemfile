60334002305d86f164548aedc9d21590d55e74a7
Uprofound <uzunmarina@mail.ru>
2020-06-30 12:27:57 +0400

    RoR_lesson5_classwork (add rendering and paginating)

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 
 gem 'slim-rails'
+gem 'kaminari'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   root 'home#index'
   get '/home(/:hello)', to: 'home#index'
+  get 'posts/page/(:page(.:format))', to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
