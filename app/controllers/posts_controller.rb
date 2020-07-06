59df8d88d88442fd78f8a6af79a29ebd839911a5
Uprofound <uzunmarina@mail.ru>
2020-07-06 15:32:35 +0400

    RoR_lesson5_homework (add paginating for post comments)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post_comments = @post.comments.order(:created_at).page(params[:page]).per(3)
   end
 
   # GET /posts/new
