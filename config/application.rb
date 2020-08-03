076b39b7a262dd422882a527f9af560ad613cbd3
Uprofound <uzunmarina@mail.ru>
2020-08-03 19:56:37 +0400

    RoR_lesson6_classwork (locales tuning)

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
