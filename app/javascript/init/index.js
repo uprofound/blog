9c6acf15d6bc17c111b5067c382b38b344a6d2da
Uprofound <uzunmarina@mail.ru>
2020-09-25 12:53:43 +0400

    RoR_lesson7_homework (Webpacker-tuning, Bootstrap-starting)

--- app/javascript/init/index.js
+++ app/javascript/init/index.js
@@ -1,4 +1,6 @@
 import "./index.css"
+import "components/home";
+import "components/about/about";
 
 window.onload = function() {
   document.body.insertAdjacentHTML("afterbegin", "Hello Webpack!");
